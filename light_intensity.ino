3086ca20fc2bc23bced1ecdede5efc6b4126a675
SakthiArumugam642 <87819058+SakthiArumugam642@users.noreply.github.com>
2023-08-27 21:47:43 +0530

    light_intensity.ino

--- light_intensity.ino
+++ light_intensity.ino
@@ -1,14 +1,40 @@
-Int sensorValue = 0;
-Void setup()
-{
-pinMode(A0, INPUT);
+int sensorvalue = A0;
+
+void setup() {
+
 Serial.begin(9600);
-pinMode(9, OUTPUT);
+
+pinMode(13, OUTPUT);
+
+pinMode(sensorvalue, INPUT);
+
 }
-Void loop()
-{
-sensorValue = analogRead(A0);
-Serial.println(sensorValue);
-analogWrite(9, map(sensorValue,0,1023,0,255));
-delay(100);
+
+void loop() {
+
+int ldr = analogRead(sensorvalue);
+
+if (ldrStatus <= 257) { //treshold value i.e 257
+
+digitalWrite(13, HIGH); sending pulse to turn on/off led
+
+Serial.print("low intensity");
+
+Serial.println(ldr); //printing the ldr value in the range of 0-1024
+
+analogWrite(13, map(sensorvalue,0,1023,0,255)); // give signal to multimeter to show the value of intensity in terms of volts. 
+
+}
+else {
+
+digitalWrite(13, LOW);
+
+Serial.print("high intensity");
+
+Serial.println(ldr);
+
+analogWrite(13, map(sensorvalue,0,1023,0,255));
+
+}
+
 }
